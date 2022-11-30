ff1729fe8e0e5c571e9914c5b2c0726ddcf1646a
Hazem Adel <74595174+hazemadelkhalel@users.noreply.github.com>
2022-11-30 15:48:55 +0200

    Update Authentication.java

--- Fawry System Eclipse/src/Authentication.java
+++ Fawry System Eclipse/src/Authentication.java
@@ -5,48 +5,17 @@ public class Authentication {
     Authentication(){
         database = new Database();
     }
-    boolean checkAccountLogin(Account client1, Client client2){
-        if(client1.getUsername().equals(client2.getUsername()) &&
-                client1.getEmail().equals(client2.getEmail()) &&
-                client1.getPassword().equals(client2.getPassword())) {
-            return true;
-        }
-        else{
-            return false;
-        }
+    void checkAccountLogin(Account client1, Client client2){
+       
     }
-    boolean checkAccountSignUp(Account client1, Client client2){
-        if(client1.getUsername().equals(client2.getUsername()) ||
-                client1.getEmail().equals(client2.getEmail())) {
-            return false;
-        }
-        else{
-            return true;
-        }
+    void checkAccountSignUp(Account client1, Client client2){
+      
     }
-    String validateLogin(Client client) {
-        for(Map.Entry <Account, String> entry : database.accounts.entrySet()){
-            Account account = entry.getKey();
-            if(checkAccountLogin(account, client)){
-                // Can Login and return the type of account
-                // admin or clinet
-                return entry.getValue();
-            }
-        }
-        // -1 represent there is no user with this information in system
-        return "-1";
+    void validateLogin(Client client) {
+       
     }
-    String validateSignUp(Client client) {
-        for(Map.Entry <Account, String> entry : database.accounts.entrySet()){
-            Account account = entry.getKey();
-            if(checkAccountSignUp(account, client)){
-                // -1 represent there is matching with other client's username or email in system
-                // Can Login and return the type of account
-                // admin or clinet
-                return "-1";
-            }
-        }
-        return "OK";
+    void validateSignUp(Client client) {
+        
     }
     void validateCreditCard(CreditCard creditCard) {
         // check if it was correct or not
